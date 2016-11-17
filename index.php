5ed6cb2e64cf8ad4da53f619137efb60f8e00d17
philharle <philharle@gmail.com>
2016-11-17 14:26:37 +0000

    Fixes

--- index.php
+++ index.php
@@ -6,10 +6,13 @@
       font-family: calibri;
     }
     
-    table,
-    th,
-    td {
+    table {
       border: 1px solid black;
+    }
+    
+    td,
+    th {
+      border: 1px solid lightgrey;
     }
   </style>
 </head>
@@ -46,14 +49,13 @@ if ($result->num_rows > 0)
 else
 {
     //No Boost in progress at the moment, so show the form
-    echo "<form action=\"boost_trigger.php\"method=\"post\">Set boost for <select name=\"boostTime\"><option value=\"30\">30 mins</option><option value=\"60\">60 mins</option><option value=\"90\">90 mins</option><option value=\"120\">120 mins</option></select><br><input type=\"submit\" value=\"Submit\"></form>";
+    echo "<form action=\"boost_trigger.php\"method=\"post\">Dry washing for <select name=\"boostTime\"><option value=\"30\">30 mins</option><option value=\"60\">60 mins</option><option value=\"90\">90 mins</option><option value=\"120\">120 mins</option></select><br><br><input type=\"submit\" value=\"Activate Boost\"></form>";
     }
 
 //Output Boost History
 echo "<br><hr><h3>Boost History:</h3>";
 $result = mysqli_query($con, "SELECT startTime, totalMins FROM boost ORDER by startTime DESC");
-echo "<table cellpadding=\"5\">";
-echo "<tr><td><b>Start time</b></td><td align=\"right\"><b>Mins</b></td></tr>";
+echo "<table><tr><td><b>Start time</b></td><td align=\"right\"><b>Mins</b></td></tr>";
 while ($row = mysqli_fetch_array($result)) {
     echo "<tr><td>" . $row['startTime'] . "</td><td align=\"right\"> " . $row['totalMins'] . "</td></tr>"; //these are the fields that you have stored in your database table employee
 }
